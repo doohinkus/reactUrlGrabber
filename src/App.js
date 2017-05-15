c1fe1105e4ef42a87e43c25f861cd40fe67f74cd
doohinkus <doohinkus@gmail.com>
2017-05-15 15:49:38 -0700

    add meta tags

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import MetaTags from 'react-meta-tags';
 //grab parameter from url
 function getParameterByName(name, url) {
   if (!url) url = window.location.href;
@@ -14,12 +15,14 @@ class App extends Component {
     var postcard = getParameterByName('postcard');
     return (
       <div className="App">
-        <meta name="twitter:card" content="summary_large_image"></meta>
-        <meta name="twitter:site" content="@baresolerunner"></meta>
-        <meta name="twitter:creator" content="@doohinkus"></meta>
-        <meta name="twitter:title" content="Check out this amazing postcard"></meta>
-        <meta name="twitter:description" content="One of many photo postcards in our international postcard exchange."></meta>
-        <meta name="twitter:image" content={postcard}></meta>
+        <MetaTags>
+          <meta name="twitter:card" content="summary_large_image"></meta>
+          <meta name="twitter:site" content="@baresolerunner"></meta>
+          <meta name="twitter:creator" content="@doohinkus"></meta>
+          <meta name="twitter:title" content="Check out this amazing postcard"></meta>
+          <meta name="twitter:description" content="One of many photo postcards in our international postcard exchange."></meta>
+          <meta name="twitter:image" content={postcard}></meta>
+        </MetaTags>
         {postcard}
       </div>
     );
