1d0640e3f049dac20a71e4974aadd7940aa4c120
doohinkus <doohinkus@gmail.com>
2017-05-15 15:25:36 -0700

    make it functional

--- src/App.js
+++ src/App.js
@@ -15,12 +15,11 @@ class App extends Component {
     return (
       <div className="App">
         <meta name="twitter:card" content="summary_large_image"></meta>
-        <meta name="twitter:site" content="@nytimes"></meta>
-        <meta name="twitter:creator" content="@SarahMaslinNir"></meta>
-        <meta name="twitter:title" content="Parade of Fans for Houston’s Funeral"></meta>
-        <meta name="twitter:description" content="NEWARK - The guest list and parade of limousines with celebrities emerging from them seemed more suited to a red carpet event in Hollywood or New York than than a gritty stretch of Sussex Avenue near the former site of the James M. Baxter Terrace public housing project here."></meta>
-        <meta name="twitter:image" content="http://graphics8.nytimes.com/images/2012/02/19/us/19whitney-span/19whitney-span-articleLarge.jpg"></meta>
-
+        <meta name="twitter:site" content="@baresolerunner"></meta>
+        <meta name="twitter:creator" content="@doohinkus"></meta>
+        <meta name="twitter:title" content="Check out this amazing postcard"></meta>
+        <meta name="twitter:description" content="One of many photo postcards in our international postcard exchange."></meta>
+        <meta name="twitter:image" content={postcard}></meta>
         {postcard}
       </div>
     );
